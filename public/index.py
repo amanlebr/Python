47dcf66d341159f5e5132833c92ef8e96b9aa01b
Leonardo Amancio <amancio.leonardo@gmail.com>
2020-05-31 09:29:11 -0300

    modified file index.py

---
+++ public/index.py
@@ -0,0 +1,26 @@
+
+"""
+Autor: Leonardo Amancio
+Data: 
+versão: 03 (Utilizdo a condição elif)
+Descrição: Faça um programa que leia um numero e exiba o dia correspindente da semana. 
+(1 - Domingo, 2 - segunda,  3 - Terça ...), se digitrar outro valor, deve aparecer valor inválido.
+"""
+
+dia = int(input('Digite o dia da semana: ')) 
+if dia == 1:
+	print("Domingo")
+elif dia == 2:
+	print("Segunda")
+elif dia == 3:
+	print("Terça")
+elif dia == 4:
+	print("Quarta")
+elif dia == 5:
+	print("Quinta")
+elif dia == 6:
+	print("Sexta")
+elif dia == 7:
+	print("Sábado")
+else: 
+	print("Dia invalido") 
